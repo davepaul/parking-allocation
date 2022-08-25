3bf16be2d459c21e37428119b4b4d049a2c53cfa
davepaul <garcia.davepaul@gmail.com>
2022-08-25 08:49:22 +0800

    Add Head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ParkingMap from "../components/parking/Map";
 import ParkingForm from "../components/parking/Form";
+import Head from "next/head";
 
 const Parking = () => {
   return (
     <div className="max-w-[50%] mx-auto my-18">
+      <Head>
+        <title>Parking Allocation</title>
+      </Head>
       <section className="sticky top-0 bg-[#000]">
         <ParkingForm />
       </section>
